16a3755d95202bc5e156469cab914c1c6699c938
Stanley <jeffywang1994@gmail.com>
2011-09-22 10:59:50 -0400

    removed spacing and declared variable types

--- Assignments/04 Counting with Pineapple.c
+++ Assignments/04 Counting with Pineapple.c
@@ -8,39 +8,29 @@ int countFunction1(int amountPineapple, int amountOrange);
 int countFunction2(int amountPineapple, int amountOrange);
 
 int main(int argc, char *argv[]) {
-
 	//initializes the amount of pineapples and oranges
 	int amountPineapple = 0;
 	int amountOrange = 0;
 	
 	int samePinneppleOrangeAmount = countFunction1(amountPineapple, amountOrange);
-	//prints the amount of pineapples and oranges
+	// prints the amount of pineapples and oranges
 	printf("You have %d pineapples and oranges\n", samePinneppleOrangeAmount);
 	
 	return 0;
-
 }
 
-int countFunction1(amountPineapple, amountOrange) {
-
+int countFunction1(int amountPineapple, int amountOrange) {
 	if (amountPineapple < 10) {
 		// goes to 2nd function and adds one pineapple
 		return countFunction2(amountPineapple + 1, amountOrange);
-	
 	}
-	
 	return amountPineapple;
-
 }
 
-int countFunction2(amountPineapple, amountOrange) {
-		
+int countFunction2(int amountPineapple, int amountOrange) {
 	if (amountOrange < 5) {
 		//goes to 1st function and adds one orange
 		return countFunction1(amountPineapple, amountOrange + 1);
-	
 	}
-	
 	return amountOrange;
-
 }
