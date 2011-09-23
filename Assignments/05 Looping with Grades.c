caa371d5b054ca4dee98984c2778206a74d328a9
Stanley <jeffywang1994@gmail.com>
2011-09-23 12:57:40 -0400

    looping with grapes and cucumbers complete

--- Assignments/05 Looping with Grades.c
+++ Assignments/05 Looping with Grades.c
@@ -1,23 +1,30 @@
 // Author:		Stanley Wang
 // Created:		9/22/11
 // Modified:	9/22/11
+// counting with grapes and cucumbers 
 
+// include standard input/output as header
 #include <stdio.h>
 
+// Caps the total cucumbers and grape amount
 #define TOTAL_CUCUMBERS 40
 #define TOTAL_GRAPES 50
 
 int main(int argc, char *argv[]) {
-	//counting with grapes and cucumbers 
 	
+	// initializes and declares variables
 	int amountGrapes = 0;
 	int amountCucumbers = 0;
-	
+
+	// checks to see if the amount of cucumbers or amount of grapes is less than the total that was defined
+	// keeps on checking until the amount is equal to the defined amount
 	while ((amountCucumbers < TOTAL_CUCUMBERS) && (amountGrapes < TOTAL_GRAPES)) {
-		amountCucumber++;
+		// adds one cucumber and then one grape
+		amountCucumbers++;
 		amountGrapes++;
 	}
 	
-	printf("You have %d cucumbers and %d grapes", amountCucumbers, amountGrapes)
+	// prints out a message of when # the cucumber and grape amount will be equal
+	printf("You have %d cucumbers and %d grapes", amountCucumbers, amountGrapes);
 	return 0;
 }
